a54fd8d4576dcdd7df8e04c362ba81f36aa05d25
Anton Ladan <ladananton@proton.me>
2024-03-08 09:11:07 +0100

    Change indent width to 2 spaces
    I prefer 4 normally, but 2 is so widely used that I simply gave in

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,7 @@
 vim.opt.clipboard = "unnamed"
 vim.opt.scrolloff = 6
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 2
 vim.opt.sidescrolloff = 12
 vim.opt.spelllang = { "en", "hr" }
-vim.opt.tabstop = 4
+vim.opt.tabstop = 2
 vim.opt.colorcolumn = "80"
