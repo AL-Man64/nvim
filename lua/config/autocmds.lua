f261ffd84d9f473b650a8627fc25ad1d26386928
Anton Ladan <ladananton450@outlook.com>
2024-07-08 22:43:02 +0200

    remove some autocmds

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,4 @@
+vim.api.nvim_del_augroup_by_name("lazyvim_highlight_yank")
+vim.api.nvim_del_augroup_by_name("lazyvim_last_loc")
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_del_augroup_by_name("lazyvim_json_conceal")
